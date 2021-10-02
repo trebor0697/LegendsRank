6e7781dbfa666b71ac8f7a546180ae53dd3e8bce
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-10-01 22:50:41 -0400

    code restructured

--- LegendsRank/LegendsRank/Services/SummonerAPIService.cs
+++ LegendsRank/LegendsRank/Services/SummonerAPIService.cs
@@ -11,31 +11,28 @@ using System.Collections.Generic;
 
 namespace LegendsRank.Services
 {
-    class SummonerAPIService
+    public class SummonerAPIService : ISummonerAPIService
     {
-        public class SummonerAPIService : ISummonerAPIService
+        public async Task<Summoner> GetSummonerAsync(string summonerName)
         {
-            public async Task<Summoner> GetSummonerAsync(string summonerName)
+            Summoner summ = null;
+
+            HttpClient client = new HttpClient();
+
+            var SummonerName = summonerName.Replace(" ", "%20");
+
+            var urlString = $"https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}?api_key=RGAPI-e40aa00c-2072-436c-9002-3fc07374cafd";
+
+
+
+            var response = await client.GetAsync(urlString);
+
+            if (response.IsSuccessStatusCode)
             {
-                Summoner summ = null;
-
-                HttpClient client = new HttpClient();
-
-                var SummonerName = summonerName.Replace(" ", "%20");
-
-                var urlString = $"https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}?api_key=RGAPI-e40aa00c-2072-436c-9002-3fc07374cafd";
-
-
-
-                var response = await client.GetAsync(urlString);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    summ = JsonConvert.DeserializeObject<Summoner>(await response.Content.ReadAsStringAsync());
-                }
-
-                return summ;
+                summ = JsonConvert.DeserializeObject<Summoner>(await response.Content.ReadAsStringAsync());
             }
+
+            return summ;
         }
     }
 }
